be851f0b24c1c0e9e58ddb06ae9babadd72e4c5f
InkerBot <im@inker.bot>
2025-04-03 06:22:20 +0800

    Method Locator should dfs

--- src/main/java/bot/inker/krypix/asm/locator/MethodLocator.java
+++ src/main/java/bot/inker/krypix/asm/locator/MethodLocator.java
@@ -6,11 +6,11 @@ import bot.inker.krypix.KrypixMethod;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class MethodLocator {
+  @SuppressWarnings("StringOperationCanBeSimplified") // we need to create new string
   private static final String ANY = new String("*");
 
   private final AppView appView;
@@ -52,15 +52,22 @@ public class MethodLocator {
       : appView.getClasses(owner);
   }
 
+  private void collectHierarchyDFS(KrypixClass owner, List<KrypixClass> visitedClasses) {
+    visitedClasses.add(owner);
+    if (owner.superClass() != null) {
+      collectHierarchyDFS(owner.superClass(), visitedClasses);
+    }
+    for (KrypixClass anInterface : owner.interfaces()) {
+      collectHierarchyDFS(anInterface, visitedClasses);
+    }
+  }
+
   private Stream<KrypixClass> collectSuperClassesIfNeed(KrypixClass owner) {
     if (exactOwner) {
       return Stream.of(owner);
     }
     List<KrypixClass> classes = new ArrayList<>();
-    classes.add(owner);
-    for (KrypixClass t = owner; t.superClass() != null; t = t.superClass()) {
-      classes.add(t.superClass());
-    }
+    collectHierarchyDFS(owner, classes);
     return classes.stream();
   }
 
@@ -73,12 +80,5 @@ public class MethodLocator {
 
   public @Nullable KrypixMethod firstOrNull() {
     return locate().findFirst().orElse(null);
-  }
-
-  private static class DescComparator implements Comparator<KrypixMethod> {
-    @Override
-    public int compare(KrypixMethod o1, KrypixMethod o2) {
-      return Integer.compare(o1.desc().length(), o2.desc().length());
-    }
   }
 }
