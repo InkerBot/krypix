1fa964952deaf61cda5e35cca675ccce875eaedd
InkerBot <im@inker.bot>
2025-04-04 06:39:28 +0800

    add a very dangerous dictionary

--- src/main/java/bot/inker/krypix/common/directory/DictionaryMaker.java
+++ src/main/java/bot/inker/krypix/common/directory/DictionaryMaker.java
@@ -1,6 +1,7 @@
 package bot.inker.krypix.common.directory;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public final class DictionaryMaker {
   private DictionaryMaker() {
@@ -54,30 +55,20 @@ public final class DictionaryMaker {
     );
   }
 
-  public static NameFactory createUtf16() {
-    /*
-    List<String> utf16NotUtf8Chars = new ArrayList<>();
-
-    for (int i = Character.MAX_VALUE; i < Integer.MAX_VALUE; i++) {
-      switch (Character.getType(i)) {
-        case Character.CONTROL:
-        case Character.FORMAT: {
-          utf16NotUtf8Chars.add(new String(new int[]{i}, 0, 1));
-          break;
-        }
-      }
-    }
-    */
-
-    return createAlphabet(
-      69821, 119155, 119156, 119157, 119158, 119159, 119160, 119161, 119162, 917505, 917536, 917537, 917538, 917539,
-      917540, 917541, 917542, 917543, 917544, 917545, 917546, 917547, 917548, 917549, 917550, 917551, 917552, 917553,
-      917554, 917555, 917556, 917557, 917558, 917559, 917560, 917561, 917562, 917563, 917564, 917565, 917566, 917567,
-      917568, 917569, 917570, 917571, 917572, 917573, 917574, 917575, 917576, 917577, 917578, 917579, 917580, 917581,
-      917582, 917583, 917584, 917585, 917586, 917587, 917588, 917589, 917590, 917591, 917592, 917593, 917594, 917595,
-      917596, 917597, 917598, 917599, 917600, 917601, 917602, 917603, 917604, 917605, 917606, 917607, 917608, 917609,
-      917610, 917611, 917612, 917613, 917614, 917615, 917616, 917617, 917618, 917619, 917620, 917621, 917622, 917623,
-      917624, 917625, 917626, 917627, 917628, 917629, 917630, 917631
-    );
+  public static NameFactory createUtf16NotUtf8() {
+    String[] elements = IntStream.rangeClosed(Character.MIN_CODE_POINT, Character.MAX_CODE_POINT)
+      .filter(Character::isValidCodePoint)
+      .filter(c -> c > 0x7F && c < 0xD800 || c > 0xDFFF)
+      .mapToObj(it -> new String(new int[]{it}, 0, 1))
+      .toArray(String[]::new);
+    return createElements(elements);
   }
+
+  public static NameFactory createUtf16InvalidCodePoint() {
+    String[] elements = IntStream.concat(
+      IntStream.of(0xD800),
+      IntStream.rangeClosed(0xDC00, 0xDCFF)
+    ).mapToObj(it -> new String(new int[]{it}, 0, 1))
+      .toArray(String[]::new);
+    return createElements(elements);  }
 }
