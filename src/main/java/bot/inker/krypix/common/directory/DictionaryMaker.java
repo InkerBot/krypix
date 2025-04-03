e8c185056d51d3871fcadedfa3a8e50800f9c146
InkerBot <im@inker.bot>
2025-04-04 06:46:58 +0800

    add shuffle for dangerous code generator

--- src/main/java/bot/inker/krypix/common/directory/DictionaryMaker.java
+++ src/main/java/bot/inker/krypix/common/directory/DictionaryMaker.java
@@ -1,5 +1,8 @@
 package bot.inker.krypix.common.directory;
 
+import bot.inker.krypix.common.random.ObfuscateRandom;
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -55,20 +58,35 @@ public final class DictionaryMaker {
     );
   }
 
-  public static NameFactory createUtf16NotUtf8() {
+  private static <T> void shuffle(T[] array, ObfuscateRandom random) {
+    for(int i = array.length; i > 1; --i) {
+      T temp = array[i - 1];
+      int j = random.nextInt(i);
+      array[i - 1] = array[j];
+      array[j] = temp;
+    }
+  }
+
+  public static NameFactory createUtf16NotUtf8(ObfuscateRandom random) {
     String[] elements = IntStream.rangeClosed(Character.MIN_CODE_POINT, Character.MAX_CODE_POINT)
       .filter(Character::isValidCodePoint)
       .filter(c -> c > 0x7F && c < 0xD800 || c > 0xDFFF)
       .mapToObj(it -> new String(new int[]{it}, 0, 1))
       .toArray(String[]::new);
+    if (random != null) {
+      shuffle(elements, random);
+    }
     return createElements(elements);
   }
 
-  public static NameFactory createUtf16InvalidCodePoint() {
+  public static NameFactory createUtf16InvalidCodePoint(ObfuscateRandom random) {
     String[] elements = IntStream.concat(
       IntStream.of(0xD800),
       IntStream.rangeClosed(0xDC00, 0xDCFF)
     ).mapToObj(it -> new String(new int[]{it}, 0, 1))
       .toArray(String[]::new);
+    if (random != null) {
+      shuffle(elements, random);
+    }
     return createElements(elements);  }
 }
